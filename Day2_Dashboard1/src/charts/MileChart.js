016c4e6decaf451889656a2b52b4b581012b7179
bilgihanist <71718784+bilgihanist@users.noreply.github.com>
2022-11-15 11:59:10 +0300

    dashboard
    dashboard

---
+++ Day2_Dashboard1/src/charts/MileChart.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+import { ResponsiveContainer, BarChart, Bar, XAxis, Tooltip } from "recharts";
+import mileStaticsData from '../assets/dummy-data/mileStatics';
+
+const MileChart = () => {
+  return (
+    <ResponsiveContainer width="100%">
+      <BarChart data={mileStaticsData}>
+        <XAxis dataKey="name" stroke="#2884ff" />
+        <Bar dataKey="mileStats" stroke="#2884ff" fill="#2884ff" barSize={30} />
+        <Tooltip wrapperClassName="tooltip__style" cursor={false} />
+      </BarChart>
+    </ResponsiveContainer>
+  );
+};
+
+export default MileChart;
